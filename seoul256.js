b4f0ace7e21115f802125fa91ae83bf85fdedaa6
Eugen Kammerloher <eugen.kammerloher@gmail.com>
2023-04-24 21:59:05 +0200

    Create seoul256.js

---
+++ seoul256.js
@@ -0,0 +1,26 @@
+black       = '#000000';
+red         = '#BA3C3C'; // red
+green       = '#5D8744'; // green
+yellow      = '#D5A556'; // yellow
+blue        = '#887EB4'; // blue
+magenta     = '#CD8DAA'; // pink
+cyan        = '#618B61'; // cyan
+white       = '#CFCFD1'; // light gray
+lightBlack  = '#000000'; // medium gray
+lightRed    = '#E97170'; // red
+lightGreen  = '#ABBA90'; // green
+lightYellow = '#D3C9B1'; // yellow
+lightBlue   = '#887EB4'; // blue
+lightMagenta= '#CCA7B1'; // pink
+lightCyan   = '#618B61'; // cyan
+lightWhite  = '#CACCD0'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(125, 128, 130, 0.5)');
+t.prefs_.set('foreground-color', '#CFCFD1');
+t.prefs_.set('background-color', '#262729');
